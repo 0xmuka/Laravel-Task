bd5e77b0dffd1d84c615d007c6e7dcd0b70a7cbe
0xmuka <hemasallam03@gmail.com>
2025-01-17 02:06:55 +0200

    add

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,19 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SliderController;
 use App\Http\Controllers\CategoryController;
-
-Route::resource('categories', CategoryController::class);
-Route::post('/categories/store', [CategoryController::class, 'store'])->name('categories.store');
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('dashboard');
-});
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-});//->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -24,8 +21,11 @@ Route::middleware('auth')->group(function () {
 
 Route::middleware(['auth'])->group(function () {
     Route::resource('sliders', SliderController::class);
-
 });
 
+Route::middleware(['auth'])->group(function () {
+    Route::resource('categories', CategoryController::class)->except(['store']);
+    Route::post('/categories/store', [CategoryController::class, 'store'])->name('categories.store');
+});
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
